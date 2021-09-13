04c8abd5c6893685c61a7e155b4443736beacad9
Muhammad <ummatimuhammadiyy@gmail.com>
2021-09-13 17:56:03 +0500

    dcdcdc

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,25 +1,41 @@
 @extends('voyager::master')
 <link rel="stylesheet" href="/css/chatt.css">
 @section('content')
+    <div class="main-container">
+      <div class="head-container">
+        <h1>• Mark Zuckerberg</h1>
+        <a href="#" class="btn"
+          ><img src="https://svgshare.com/i/Knn.svg" alt="close" class="delete"
+        /></a>
+      </div>
 
-<div class="chat">
-  <div class="chat-title">
-    <h1>Fabio Ottaviani</h1>
-    <h2>Supah</h2>
-    <figure class="avatar">
-      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>
-  </div>
-  <div class="messages">
-    <div class="messages-content"></div>
-  </div>
-  <div class="message-box">
-    <textarea type="text" class="message-input" placeholder="Type message..."></textarea>
-    <button type="submit" class="message-submit">Send</button>
-  </div>
+      <div class="message-container">
+        <h3><span class="date">Today</span></h3>
+        <div class="sent">
+          <h5 class="hour">10:53</h5>
+          <p class="sent-bubble">
+            Hi, Mark! I made a new design for Messenger App.
+          </p>
+        </div>
+        <div class="received">
+          <h5 class="hour">10:57</h5>
+          <p class="received-bubble">
+            Yo! Send it to my assistant and we'll review it during the year.
+          </p>
+        </div>
+        <div class="sent">
+          <h5 class="hour">11:03</h5>
+          <p class="sent-bubble">But Mark...</p>
+        </div>
+        <div class="blocked">
+          <h5 class="hour">11:05</h5>
+          <p class="blocked-bubble">You were blocked by the user</p>
+        </div>
+      </div>
 
-</div>
-<div class="bg"></div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="/js/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js">
-<script src="/js/chat.js"></script>
+      <div class="input-container">
+        <input type="text" placeholder="Enter your message" />
+        <a href="#" class="btn">Send</a>
+      </div>
+    </div>
 @endsection
