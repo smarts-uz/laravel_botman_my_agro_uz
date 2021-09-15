fbe5d83a164e72f1653c31346f8d0e330a29572c
Asadbekinha <fayzulloevasadbek@gmail.com>
2021-09-15 19:58:16 +0500

    table change

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -12,18 +12,20 @@ class ConversationController extends Controller
         return view('appeal.chat', compact('conversations', 'appeal'));
     }
     public function send($appeal, Request $request){
+
         $con = new Conversation();
         $con->user_id = $request->user()->id;
         $con->text = $request->text;
         $con->appeal_id = $appeal;
         $request->user()->role == 'user' ? $con->is_answer = 0 : $con->is_answer = 1;
-        $con->is_closed = 0;
         $con->save();
         $appeal = Appeal::where('id', $appeal)->first();
         $conversations = Conversation::where('appeal_id', $appeal->id)->get()->sortBy('created_at');
         return view('appeal.chat', compact('appeal', 'conversations'));
     }
-    public function close(){
-        
+    public function close($appeal){
+        if(Appeal::where('id', $appeal)->update(["is_closed" => 1])){
+            return redirect()->back()->with('success', 'Closed');
+        } return redirect()->back()->with('warning', 'something went wrong!');
     }
 }
