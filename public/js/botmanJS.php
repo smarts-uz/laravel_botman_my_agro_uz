07ada9b488f75792879fa5a82866d8df2eff0ed3
Gitea <gitea@fake.local>
2021-09-15 23:27:11 +0500

    133333

--- public/js/botmanJS.php
+++ public/js/botmanJS.php
@@ -14,18 +14,37 @@ $password = 'root';
 $dbname = 'laravel_agromy';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-// if ($conn->connect_error) {
-//   \Log::info("Connection failed: " . $conn->connect_error);
-// }
+
+
+/*
+ *
+ *  @php
+        $chatLogo = setting('chatbot.icon_image');
+$chatLogo = str_replace('\\', '/', $chatLogo);
+          //dd( $chatLogo);
+          //
+        //  $intro = htmlspecialchars(setting('chatbot.ChatIntro'));
+          $intro = setting('chatbot.ChatIntroText');
+    @endphp
+
+    var botmanWidget = {
+        // bubbleBackground: "blue",
+        bubbleAvatarUrl: 'https://my.agro.uz/images/logo.png',
+        frameEndpoint: "chat.html",
+        introMessage: `{{$intro}}`
+    };
+ * */
+
+
 $sql = "SELECT * FROM settings";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $arr[] = $row;
-        if ($row['key'] == 'chatbot.chat_title') $title = $row['value'];
-        if ($row['key'] == 'chatbot.chat_intro_message') $intro = $row['value'];
-        if ($row['key'] == 'chatbot.placeholder_text') $placeText = $row['value'];
-        if ($row['key'] == 'chatbot.icon_image') $icon = addslashes($row['value']);
+        if ($row['key'] === 'chatbot.chat_title') $title = $row['value'];
+        if ($row['key'] === 'chatbot.ChatIntroText') $intro = $row['value'];
+        if ($row['key'] === 'chatbot.placeholder_text') $placeText = $row['value'];
+        if ($row['key'] === 'chatbot.icon_image') $icon = addslashes($row['value']);
 
 
     }
@@ -34,19 +53,19 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 
-echo '
-            var botmanWidget = {
-            frameEndpoint: "https://my.agro.uz/package/build/chat.html",
-            bubbleAvatarUrl: "https://my.agro.uz/storage/' . ($icon) . '",
-            aboutLink: "https://agro.uz",
-	        aboutText: "Powered By Agro.Uz",
-            introMessage: "' . $intro . '",
-			title: "' . strval($title) . '",
-			placeholderText: "' . $placeText . '"
-	    };
-        ';
-echo ' t = document.getElementById("messageArea");
-        wer = document.getElementById("botmanChatRoot");
 
-        ';
+echo <<<JS
+var botmanWidget = {
+frameEndpoint: "https://my.agro.uz/package/build/chat.html",
+bubbleAvatarUrl: "https://my.agro.uz/images/logo.png",
+aboutLink: "https://agro.uz",
+aboutText: "Powered By Agro.Uz",
+introMessage: `$intro`,
+title: `$title`,
+placeholderText: `$placeText`
+};
+
+JS;
+
 ?>
+
