561b37339580b17c60aa7a92d72819312e3e64ee
Gitea <gitea@fake.local>
2021-09-17 14:38:01 +0500

    13333

--- app/Http/Controllers/FilepondController.php
+++ app/Http/Controllers/FilepondController.php
@@ -6,13 +6,22 @@ use Illuminate\Http\Request;
 
 class FilepondController extends Controller
 {
-    public function upload(Request $request) {
 
-        if($request->hasFile('file')) {
+    public function chat()
+    {
+
+    }
+
+    public function upload(Request $request)
+    {
+
+        if ($request->hasFile('file')) {
             $file = $request->file('file');
             $filename = $file->getClientOriginalName();
-            $folder = uniqid().'-'.now()->timestamp;
-            $file->storeAs($folder,$filename);
+
+            $folder = $_SERVER['DOCUMENT_ROOT'] . '/upload/';
+
+            $file->storeAs($folder, $filename);
 
             return response()->json([
                 "success" => true,
@@ -24,6 +33,35 @@ class FilepondController extends Controller
             "success" => false,
             "message" => "File unsuccessfully uploaded",
         ]);
-        
+
+    }
+
+
+    public function fileUpload(Request $req)
+    {
+        /*  $req->validate([
+              'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048'
+          ]);*/
+
+        if ($req->file()) {
+            $fileName = time() . '_' . $req->file->getClientOriginalName();
+            $filePath = $req->file('file')
+                ->storeAs('uploads', $fileName, 'public');
+
+            $fileModelname = time() . '_' . $req->file->getClientOriginalName();
+            $fileModelfile_path = '/storage/' . $filePath;
+
+            return response()->json([
+                "success" => true,
+                "message" => "File successfully uploaded",
+                "file" => $fileName
+            ]);
+        }
+
+        return response()->json([
+            "success" => false,
+            "message" => "File unsuccessfully uploaded",
+        ]);
+
     }
 }
