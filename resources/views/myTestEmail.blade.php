b925311d5908d590ca842bd5f5755be4c90de6a9
Muhammad <ummatimuhammadiyy@gmail.com>
2021-09-17 16:06:54 +0500

    add file email

--- resources/views/myTestEmail.blade.php
+++ resources/views/myTestEmail.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Online Web Tutor</title>
+    <title>AGRO</title>
 </head>
 <body>
 
 <h1>{{ $details['title'] }}</h1>
 <p>{{ $details['body'] }}</p>
 <p>Link for Cobinet : https://my.agro.uz/admin</p>
+
+    @foreach($details['files'] as $file)
+    <a href='{{url("")."/storage/".$file}}'>FILE</a>
+    @endforeach 
 
 <p>Thank you</p>
 </body>
