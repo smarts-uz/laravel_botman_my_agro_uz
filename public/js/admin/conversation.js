77da0e7676cdbc142116d3207a5a59dbd5fac7ae
arabic0513 <rustamyusupov230@gmail.com>
2021-09-20 10:13:23 +0500

    star rating and background

--- public/js/admin/conversation.js
+++ public/js/admin/conversation.js
@@ -34,10 +34,11 @@ function Rate() {
     Swal.fire({
         title: '<strong>Expertni baholang</strong>',
         icon: 'info',
-        html: `<input id="rating-input" type="text" title="" />
-                `,
         showCloseButton: true,
-        showCancelButton: true,
         focusConfirm: false,
-    })
+        confirmButtonText:
+          '<i class="rate"><input type="radio" id="star1" name="rate" value="1" /> <label for="star1" title="text">1 star</label><input type="radio" id="star2" name="rate" value="2" /> <label for="star2" title="text">2 stars</label> <input type="radio" id="star3" name="rate" value="3" /> <label for="star3" title="text">3 stars</label>  <input type="radio" id="star4" name="rate" value="4" /> <label for="star4" title="text">4 stars</label> <input type="radio" id="star5" name="rate" value="5" /> <label for="star5" title="text">5 stars</label></i>',
+        confirmButtonAriaLabel: 'Thumbs up, great!'
+        
+      })
 }
