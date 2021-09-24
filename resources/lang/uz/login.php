34a3adf764bb84c38021a4a123fe983b908c10e5
Tohir <hakimovtohirbek@gmail.com>
2021-09-24 10:36:24 +0500

    jdjd

--- resources/lang/uz/login.php
+++ resources/lang/uz/login.php
@@ -3,5 +3,5 @@
 return [
     'loggingin' 		   => 'Sistemaga kirish',
     'signin_below' 		=> 'Boshqaruv paneliga kirish',
-    'welcome' 			    => 'Laravelda yetarli bo`lmagan boshqaruv paneli',
+    'welcome' 			    => 'Boshqaruv paneli',
 ];
