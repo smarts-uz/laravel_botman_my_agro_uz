7f53a4d6abf4f11784cebd99d71a2b38cc755b56
Anvar <anvarsadinov0807@gmail.com>
2021-09-25 13:52:58 +0500

    Send chat Id

--- resources/views/myTestEmail.blade.php
+++ resources/views/myTestEmail.blade.php
@@ -10,9 +10,11 @@
 
 <h1><span>{{ $details['title'] }}</span></h1>
 <h1>Murojaat matni</h1>
+<br>
 <p>{{ $details['body'] }}</p>
+<br>
 <p>Link for Cabinet : https://my.agro.uz/admin</p>
-    
+  <p>{{ appeal->id }}</p>
 <p>Thank you</p>
 </body>
 </html>
