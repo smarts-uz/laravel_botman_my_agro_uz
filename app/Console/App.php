5574a092ea8b7aa03ab58ef852eab218dd346ea3
tohir <hakimovtohirbek@gmail.com>
2021-10-13 18:33:24 +0500

    ww

---
+++ app/Console/App.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ *
+ * Author:  Asror Zakirov
+ * https://www.linkedin.com/in/asror-zakirov
+ * https://github.com/asror-z
+ *
+ */
+
+$ch = curl_init('https://textbelt.com/text');
+$data = array(
+    'phone' => '+998974037243',
+    'message' => 'Hello worl13131d',
+    'key' => 'textbelt',);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
