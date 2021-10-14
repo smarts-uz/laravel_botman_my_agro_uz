0e8019e6cf509f826f1ae7087a0a67025891ec7e
Asadbekinha <fayzulloevasadbek@gmail.com>
2021-10-14 16:59:19 +0500

    files list

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 use TCG\Voyager\Voyager;
 class HelperController extends Controller
 {
@@ -10,5 +12,17 @@ class HelperController extends Controller
         if (file_exists(asset('/js/botmanJS.php'))) {
             require asset('/js/botmanJS.php');
         }
+    }
+    public function getAll(){
+        $text = '';
+        $path = 'uploads/fayzulloevasadbek@gmail.com/';
+        $files = Storage::allFiles($path);
+        Storage::delete($files);
+
+//        foreach ($files as $file){
+//            ;
+//            $text = $text . str_replace($path, '', $file) . '<br>';
+//        }
+//        return $text;
     }
 }
